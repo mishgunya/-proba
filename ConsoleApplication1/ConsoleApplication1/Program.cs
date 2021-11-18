a90f0f7badcefe4e1c93df52bedd6cc79b94bd8b
Misha <19fishka96@mail.ru>
2021-11-18 17:20:24 +0300

    start

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.printState();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class  Car
+    {
+        public string petName;
+        public int currSpeed;
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+        public void printState()
+            => Console.WriteLine($"{petName} is going {currSpeed}");
+
+    }
+
+
+}
